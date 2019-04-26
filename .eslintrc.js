9d3a58272677e041846014155b722c7880ddfc5d
Brian Bugh <brainbag@gmail.com>
2019-04-26 10:00:03 -0500

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  env: {
+    commonjs: true,
+    mocha: true
+  },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'prettier/@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': ['warn', { allowExpressions: true }]
+  }
+};
--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ import * as Color from 'color';
 
 type ConversionCallback = (color: Color) => string;
 
-function replaceColorTest(conversion: ConversionCallback) {
+function replaceColorTest(conversion: ConversionCallback): void {
   const editor = vscode.window.activeTextEditor;
   const { document, selections } = editor;
 
@@ -34,11 +34,7 @@ function replaceColorTest(conversion: ConversionCallback) {
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "color-space-shift" is now active!');
-
+export function activate(context: vscode.ExtensionContext): void {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with  registerCommand
   // The commandId parameter must match the command field in package.json
@@ -50,4 +46,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): void {}
